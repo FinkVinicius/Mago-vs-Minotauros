4dfaaaaf745ee5a732c9ecc60f144d3fed2cb16d
FinkVinicius <viniciusfink2001@gmail.com>
2026-04-30 18:17:16 -0300

    inclui as constantes, coloquei um laço for pra fechar o games, upei a musica nos assets coloquei a foto resposiva ao tamanho da janela no menu

--- game.py
+++ game.py
@@ -1,23 +1,27 @@
 
 import pygame
 
+from const import ALTURA_WIN, LARGURA_WIN
 from menu import Menu
 class Game:
+
     def __init__(self):
         pygame.init()
-        window = pygame.display.set_mode(size= (800, 600))
+        self.window = pygame.display.set_mode(size= (LARGURA_WIN, ALTURA_WIN))
 
     def run(self, ):
        
         while True:
+        # Esse for é pra fechar a janela quando clicar no X
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+        
             menu = Menu(self.window)
             menu.run()
             pass
 
 
 
-            #daqui pra frente é pra fechar o games
-            #for event in pygame.event.get():
-                #if event.type == pygame.QUIT:
-                    #pygame.quit()
-                    #quit()
+            
--- menu.py
+++ menu.py
@@ -1,8 +1,14 @@
 import pygame
+from const import ALTURA_WIN, LARGURA_WIN
 class Menu:
     def __init__(self, window):
         self.window = window
-
-    def run(self, ):
-        while True:
-            pass
+        
+        # pega a imagem e redimensiona ela pra caber na tela
+        self.surf = pygame.transform.scale(pygame.image.load('./Assets/menu.png'), (LARGURA_WIN, ALTURA_WIN))
+        self.rect = self.surf.get_rect()
+               
+    def run(self):
+        self.window.blit(source=self.surf, dest=self.rect)
+        pygame.display.flip()
+        pass
