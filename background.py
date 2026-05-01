8c3b0b9bf47dd10ae3628552549f051a148247c7
FinkVinicius <viniciusfink2001@gmail.com>
2026-05-01 17:11:02 -0300

    teste

--- background.py
+++ background.py
@@ -8,7 +8,7 @@ class background(entity.Entity):
         super().__init__(name, position)    
         #No meu projeto resolvi usar mais de um background para criar um efeito de paralaxe, por isso o nome do background tem um numero no final, e a velocidade do background é definida por esse numero
         self.speed = int(name[-1])
-        pass
+        pass 
 
     def move(self,):
         self.rect.centerx -= self.speed
@@ -16,3 +16,5 @@ class background(entity.Entity):
         if self.rect.right <= 0:
             self.rect.left = LARGURA_WIN
         pass
+
+    
